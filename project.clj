fd004615019e7eb3576ad20c97c43d5e78897710
Ilya Klyuchnikov <ilya.klyuchnikov@gmail.com>
2014-07-05 14:57:47 +0400

    project structure

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject clojure-logic-examples "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.logic "0.8.7"]
+                 [org.ow2.sat4j/org.ow2.sat4j.sat "2.3.5"]])
